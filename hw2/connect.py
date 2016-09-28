f2e2971589ad7fecc83855c50831a9a3e6bb2f45
WindQAQ <b03902042@ntu.edu.tw>
2016-09-28 06:03:47 -0400

    Unfinished: connect.py

--- hw2/connect.py
+++ hw2/connect.py
@@ -1,45 +1,76 @@
 import sys
 from PIL import Image
 
-def label(table, X, Y):
-	equiv = {}
+import random
+
+def Find(p, x):
+	if x == p[x]:
+		return x
+	else:
+		p[x] = Find(p, p[x])
+		return p[x]
+
+def Union(p, x, y):
+	X, Y = Find(p, x), Find(p, y)
+	p[X] = Y
+
+def Label(pix, width, height):
+	
+	p = {}
+
+	labels = {}
 	stamp = 1
-	for x in range(X):
-		for y in range(Y):
-			if table[x, y] == 0:
+
+	for y in range(height):
+		for x in range(width):
+			if pix[x, y] == 0:
+				labels[x, y] = 0
 				continue
 			
-			left = table[x-1, y] if (x-1, y) in table else 0
-			up = table[x, y-1] if (x, y-1) in table else 0
+			left = labels[x-1, y] if x-1 >= 0 else 0
+			top = labels[x, y-1] if y-1 >= 0 else 0
 			
-			if left == 0 and up == 0:
-				table[x, y] = stamp
-				equiv[stamp] = set()
+			if left == 0 and top == 0:
+				labels[x, y] = stamp
+				p[stamp] = stamp
 				stamp += 1
-			elif left != 0 and up != 0:
-				table[x, y] = left if left < up else up
-				if left != up:
-					equiv[left].add(up)
-					equiv[up].add(left)
+			elif left != 0 and top != 0:
+				labels[x, y] = left if left < top else top
+				if left != top:
+					Union(p, left, top)
 			else:
-				table[x, y] = left if left != 0 else up
-	return equiv
+				labels[x, y] = left if left != 0 else top
+	
+	out = Image.new('RGB', (width, height))
+	oo = out.load()
+	color = {}
+	# replace
+	for y in range(height):
+		for x in range(width):
+			if pix[x, y] != 0:
+				labels[x, y] = Find(p, labels[x, y])
+				if labels[x, y] in color:
+					oo[x, y] = color[labels[x, y]]
+				else:
+					color[labels[x, y]] = (random.randint(0, 256), random.randint(0, 256), random.randint(0, 256))
+					oo[x, y] = color[labels[x, y]]
+			else:
+				oo[x, y] = (0, 0, 0)
+	
+	out.show()
+
+def CC(img):
+	pix = img.load()	
+	width, height = img.size
+
+	Label(pix, width, height)
+	
 
 try:
-	original = Image.open(sys.argv[1])
+	img = Image.open(sys.argv[1])
 except:
 	print ("Fail to open", sys.argv[1])
 	exit()
 
-pix_ori = original.load()
-
-width, height = original.width, original.height
-
-# initialize table
-table = {}
-for x in range(width):
-	for y in range(height):
-		table[x, y] = pix_ori[x, y]
-
-# labeling
-equiv = label(table, width, height)
+# find connected components
+CC(img)
