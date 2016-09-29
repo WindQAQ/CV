63a3696576e4d72eb73e990ba3e2c224a9bdb2b9
WindQAQ <b03902042@ntu.edu.tw>
2016-09-28 23:29:51 -0400

    Unfinished: centroid of connected component

--- hw2/connect.py
+++ hw2/connect.py
@@ -1,22 +1,65 @@
-import sys
-from PIL import Image
+import sys, math
+from PIL import Image, ImageDraw
 
-import random
-
-def Find(p, x):
-	if x == p[x]:
-		return x
-	else:
-		p[x] = Find(p, p[x])
-		return p[x]
-
-def Union(p, x, y):
-	X, Y = Find(p, x), Find(p, y)
-	p[X] = Y
-
-def Label(pix, width, height):
+class DisJoint:
+	def __init__(self):
+		self.p = {}
+		self.size = {}
 	
-	p = {}
+	def make(self, x):
+		self.p[x] = x
+		self.size[x] = 1
+
+	def find(self, x):
+		if x == self.p[x]:
+			return x
+		else:
+			self.p[x] = self.find(self.p[x])
+			return self.p[x]
+
+	def union(self, x, y):
+		X, Y = self.find(self.p[x]), self.find(self.p[y])
+		if X == Y:
+			return
+
+		if self.size[X] >= self.size[Y]:
+			self.size[X] += self.size[Y]
+			self.p[Y] = X
+		else:
+			self.size[Y] += self.size[X]
+			self.p[X] = Y
+
+	def getsize(self, x):
+		return self.size[self.find(x)]
+
+	def increase(self, x):
+		self.size[self.find(x)] += 1
+
+class Component:
+	def __init__(self):
+		self.up = self.left = float('inf')
+		self.low = self.right = -float('inf')
+		self.X = 0
+		self.Y = 0
+		self.A = 0
+
+	def add(self, x, y):
+		self.up = min(self.up, y)
+		self.low = max(self.low, y)
+		self.left = min(self.left, x)
+		self.right = max(self.right, x)
+		self.X += x	
+		self.Y += y
+		self.A += 1
+
+	def getbound(self):
+		return [self.left, self.up, self.right, self.low]
+
+def Label(img):
+	pix = img.load()	
+	width, height = img.size
+	
+	Eq = DisJoint()
 
 	labels = {}
 	stamp = 1
@@ -24,7 +67,7 @@ def Label(pix, width, height):
 	for y in range(height):
 		for x in range(width):
 			if pix[x, y] == 0:
-				labels[x, y] = 0
+				labels[x, y] = 0 # background
 				continue
 			
 			left = labels[x-1, y] if x-1 >= 0 else 0
@@ -32,45 +75,56 @@ def Label(pix, width, height):
 			
 			if left == 0 and top == 0:
 				labels[x, y] = stamp
-				p[stamp] = stamp
+				Eq.make(stamp)
 				stamp += 1
 			elif left != 0 and top != 0:
 				labels[x, y] = left if left < top else top
 				if left != top:
-					Union(p, left, top)
+					Eq.union(left, top)
+				Eq.increase(labels[x, y])
 			else:
 				labels[x, y] = left if left != 0 else top
+				Eq.increase(labels[x, y])
 	
-	out = Image.new('RGB', (width, height))
-	oo = out.load()
-	color = {}
-	# replace
+	return Eq, labels
+
+def Last(img, Eq, labels):
+	width, height = img.size
+	
+	out = Image.new('RGB', img.size)
+	pix = out.load()
+	
+	Group = {}
 	for y in range(height):
 		for x in range(width):
-			if pix[x, y] != 0:
-				labels[x, y] = Find(p, labels[x, y])
-				if labels[x, y] in color:
-					oo[x, y] = color[labels[x, y]]
-				else:
-					color[labels[x, y]] = (random.randint(0, 256), random.randint(0, 256), random.randint(0, 256))
-					oo[x, y] = color[labels[x, y]]
+			if labels[x, y] != 0:
+				n = Eq.find(labels[x, y])
+				if Eq.getsize(n) >= 500:
+					if n not in Group:
+						Group[n] = Component()
+					Group[n].add(x, y)
+				pix[x, y] = (255, 255, 255)
 			else:
-				oo[x, y] = (0, 0, 0)
-	
-	out.show()
+				pix[x, y] = (0, 0, 0)
+
+	draw = ImageDraw.Draw(out)
+	color = (220, 116, 246)
+	for i in Group:
+		draw.rectangle(Group[i].getbound(), fill=None, outline=color)
+
+	out.save(sys.argv[2], 'bmp')
 
 def CC(img):
-	pix = img.load()	
-	width, height = img.size
+	Eq, labels = Label(img)
+	Last(img, Eq, labels)
 
-	Label(pix, width, height)
-	
 
-try:
-	img = Image.open(sys.argv[1])
-except:
-	print ("Fail to open", sys.argv[1])
-	exit()
+if __name__ == '__main__':
+	try:
+		img = Image.open(sys.argv[1])
+	except:
+		print ("Fail to open", sys.argv[1])
+		exit()
 
-# find connected components
-CC(img)
+	# find connected components
+	CC(img)
