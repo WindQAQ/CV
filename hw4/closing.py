fe33013c1d5f16570a362d926ac85bbb6a661da2
WindQAQ <b03902042@ntu.edu.tw>
2016-10-26 14:58:52 +0800

    finish closing

---
+++ hw4/closing.py
@@ -0,0 +1,43 @@
+import sys
+from PIL import Image
+
+kernel = [(-1, -2), (0, -2), (1, -2), 
+(-2, -1), (-1, -1), (0, -1), (1, -1), (2, -1),
+(-2,  0), (-1,  0), (0,  0), (1,  0), (2,  0),
+(-2,  1), (-1,  1), (0,  1), (1,  1), (2,  1),
+		  (-1,  2), (0,  2), (1,  2)]
+
+try:
+	img = Image.open(sys.argv[1])
+except:
+	print ("Fail to open", sys.argv[1])
+	exit()
+
+imgC = Image.new(img.mode, img.size, color=255)
+width, height = img.size
+
+pix, pixC = img.load(), imgC.load()
+
+sumTuple = lambda a, b: (a[0]+b[0], a[1]+b[1]) # element-wise add two tuples
+
+kernelR = [(-x, -y) for x, y in kernel]
+pixT = {(x, y): 0 for x in range(width) for y in range(height)} # temporary pixel map of erosion
+for x in range(width):
+	for y in range(height):
+		for _ in kernel:
+			try:
+				if pix[x, y] == 255:
+					pixT[sumTuple((x, y), _)] = 255
+			except IndexError:
+				pass
+
+for x in range(width):
+	for y in range(height):
+		for _ in kernelR:
+			try:
+				if pixT[x, y] == 0:
+					pixC[sumTuple((x, y), _)] = 0
+			except IndexError:
+				pass
+
+imgC.save(sys.argv[2], 'bmp')
