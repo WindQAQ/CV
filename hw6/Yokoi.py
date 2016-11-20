d58fb5bca57abb150d5102666e90f5ae16ba4fd8
b03902042 <b03902042@linux4.csie.ntu.edu.tw>
2016-11-20 10:57:21 +0800

    init yokoi

--- hw6/Yokoi.py
+++ hw6/Yokoi.py
@@ -2,13 +2,17 @@ import sys
 from itertools import product
 from PIL import Image
 
-BLOCK_LEN = 8
-
-def downsample(pix, size):
+def downsample(pix, size, BLOCK_LEN):
 	width, height = size[0] / BLOCK_LEN, size[1] / BLOCK_LEN
-	calPos = lambda x: x[0]*8, x[1]*8
+	calPos = lambda x: x[0]*BLOCK_LEN, x[1]*BLOCK_LEN
 	M = {pix[calPos(_)] for _ in product(range(width), range(height))}
-	return M
+	return M, (width, height)
+
+def Yokoi(M, size):
+	BLOCK_1 = [(0, 0), ( 1,  0), ( 1, -1), ( 0, -1)]
+	BLOCK_2 = [(0, 0), ( 0, -1), (-1, -1), (-1,  0)]
+	BLOCK_3 = [(0, 0), (-1,  0), (-1,  1), ( 0,  1)]
+	BLOCK_4 = [(0, 0), ( 0,  1), ( 1,  1), ( 1,  0)]
 
 def main():
 	if len(sys.argv) != 3:
@@ -22,8 +26,11 @@ def main():
 
 	size = imgIn.size
 	pixIn= imgIn.load()
+	BLOCK_LEN = 8
 
-	M = downsample(pixIn, size)
+	M, shrinkSize = downsample(pixIn, size, BLOCK_LEN)
+
+	Y = Yokoi(M, shrinkSize)
 
 if __name__ == '__main__':
 	main()
